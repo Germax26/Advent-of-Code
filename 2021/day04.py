469ae727dcd5e6dc54b9d1c79fd0a0ccee994dbe
Germax26 <maximburykin1@gmail.com>
2021-12-05 01:05:58 +1100

    Day 04 Part 2

--- 2021/day04.py
+++ 2021/day04.py
@@ -13,6 +13,9 @@ called = []
 
 boards = [[[int(y) for y in x.split()] for x in puzzle_input[n:n+5]] for n in range(1, len(puzzle_input), 5)]
 
+def score(call, board):
+	return sum([sum([x for x in row if x not in called]) for row in board]) * call
+
 def solve1():
 	for call in order:
 		called.append(call)
@@ -32,10 +35,35 @@ def solve1():
 						break
 				else:
 					continue
-			return sum([sum([x for x in row if x not in called]) for row in board]) * call
+			return score(call, board)
 
 def solve2():
-	pass
+	while called: called.pop()
+	left = set(range(len(boards)))
+	for call in order:
+		called.append(call)
+		for i, board in enumerate(boards):
+			if i not in left:
+				continue
+			for row in board:
+				for number in row:
+					if number not in called:
+						break
+				else:
+					break
+			else:
+				for j in range(5):
+					for row in board:
+						if row[j] not in called:
+							break
+					else:
+						break
+				else:
+					continue
+			if len(left) > 1:
+				left.remove(i)
+			else:
+				return score(call, board)
 	    
 print("Pt1:", solve1())
-# print("Pt2:", solve2())
+print("Pt2:", solve2())
