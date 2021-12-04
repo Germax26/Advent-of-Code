d0513c43ba4f3622d8205210fbdac1143088eea3
Germax26 <maximburykin1@gmail.com>
2021-12-04 20:16:36 +1100

    Day 02 Part 1

---
+++ 2021/day02.py
@@ -0,0 +1,29 @@
+# Dive !
+
+def read_from_file(file_name):
+    _file = open(file_name, 'r')
+    _read = [x for x in _file.read().split('\n') if x]
+    _file.close()
+    return _read
+
+puzzle_input = read_from_file("inputs/day02.txt")
+
+def solve1():
+	position = 0
+	depth = 0
+	for instruction in puzzle_input:
+		cmd, val = instruction.split()
+		val = int(val)
+		if cmd == "forward":
+			position += val
+		elif cmd == "down":
+			depth += val
+		elif cmd == "up":
+			depth -= val
+		else:
+			raise Exception(f"Unknown command: {cmd}.")
+
+	return position * depth
+
+print("Pt1:", solve1())
+# print("Pt2:", solve2())
