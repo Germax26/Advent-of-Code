d7308212d01d153504830c6ee362b0667a688ec0
Germax26 <maximburykin1@gmail.com>
2021-12-09 17:22:31 +1100

    Day 09 Part 2

--- 2021/day09.py
+++ 2021/day09.py
@@ -22,7 +22,47 @@ def solve1():
 	return total
 
 def solve2():
-	pass
+	flow = []
+	basins = []
+	for j in range(len(puzzle_input)):
+		flow.append([])
+		for i in range(len(puzzle_input[j])):
+			x = (0, 0)
+			h = puzzle_input[j][i]
 
-print("Pt1:", solve1())
-# print("Pt2:", solve2())
+			if h != 9:
+				for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+					if i + dx >= 0 and j + dy >= 0 and i + dx < len(puzzle_input[j]) and j + dy < len(puzzle_input):
+						if puzzle_input[j + dy][i + dx] < h:
+							x = (dx, dy)
+			else:
+				x = 9
+
+			if x == (0, 0):
+				basins.append((i, j))
+			flow[j].append(x)
+
+	def calculate_area(x, y):
+		area = 1
+		for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+			if x + dx >= 0 and y + dy >= 0 and x + dx < len(puzzle_input[y]) and y + dy < len(puzzle_input):
+				if puzzle_input[y + dy][x + dx] == 9:
+					continue
+				if flow[y + dy][x + dx] == (-dx, -dy):
+					area += calculate_area(x + dx, y + dy)
+		return area
+
+	areas = []
+
+	for basin in basins:
+		areas.append(calculate_area(*basin))
+
+	total = 1
+	for i in sorted(areas)[-3:]:
+		total *= i
+		
+	return total
+
+
+# print("Pt1:", solve1())
+print("Pt2:", solve2())
